3dc1e92f04104ed809dc6533d60dce4ecf49ae47
aho4ahoaho <aho4ahoaho@outlook.jp>
2019-08-31 14:33:15 +0900

    update

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-/*localStorage.setItem("test", "value");
-localStorage.setItem("test2", "value2");
-localStorage.setItem("日本語への対応は", "大丈夫そうだね");
-localStorage.setItem("list", "test;test2;日本語への対応は");*/
 'use strict';
 let title;
+let new_title;
+
 
 function list_view(){
     while (document.getElementById("list").firstChild) {
@@ -19,15 +17,23 @@ function list_click(value) {//リストがクリックされた場合にtitleの
     title = value.id;//現在開いているファイルのキーをグローバル変数に残しておく
     document.getElementById("titlebox").value = title;
     document.getElementById("textbox").value = localStorage.getItem(title);
+    new_title = false;
+}
+function new_list(){
+    document.getElementById("titlebox").value = ""
+    document.getElementById("textbox").value = ""
+    new_title = true;
 }
 
-function submit(){
+function save(){
     let titlebox = document.getElementById("titlebox").value;
     let textbox = document.getElementById("textbox").value;//入力されているデータを取得する。
     let list = localStorage.getItem("list");
     list +=titlebox+";";//リストにタイトルを追記する。
-    localStorage.removeItem(title);//前回の分のデータを一度削除する。
-    list = list.replace(title+";","");//前回の分のキーも削除する。
+    if(new_title != true){
+        localStorage.removeItem(title);//前回の分のデータを一度削除する。
+        list = list.replace(title+";","");//前回の分のキーも削除する。
+    }
     localStorage.setItem("list",list);//リストを更新する。
     localStorage.setItem(titlebox,textbox);//タイトルをキーに本文を保存する。
     list_view()//リストをリロードする。
